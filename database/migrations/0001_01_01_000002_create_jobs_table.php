b3dd0d3e76158e75174f75e46c6af97304f3eaf2
ferly.jeremi <ferly.jeremi@mncbank.co.id>
2024-08-07 11:32:51 +0700

    admin LTE done

--- database/migrations/0001_01_01_000002_create_jobs_table.php
+++ database/migrations/0001_01_01_000002_create_jobs_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('jobs', function (Blueprint $table) {
             $table->id();
-            $table->string('queue')->index();
+            $table->string('queue', 191)->index(); // Ubah panjang kolom menjadi 191 karakter
             $table->longText('payload');
             $table->unsignedTinyInteger('attempts');
             $table->unsignedInteger('reserved_at')->nullable();
@@ -22,21 +22,21 @@ return new class extends Migration
         });
 
         Schema::create('job_batches', function (Blueprint $table) {
-            $table->string('id')->primary();
-            $table->string('name');
+            $table->string('id', 191)->primary(); // Ubah panjang kolom menjadi 191 karakter
+            $table->string('name', 191); // Ubah panjang kolom menjadi 191 karakter
             $table->integer('total_jobs');
             $table->integer('pending_jobs');
             $table->integer('failed_jobs');
             $table->longText('failed_job_ids');
             $table->mediumText('options')->nullable();
             $table->integer('cancelled_at')->nullable();
-            $table->integer('created_at');
-            $table->integer('finished_at')->nullable();
+            $table->unsignedInteger('created_at'); // Ubah menjadi unsignedInteger
+            $table->unsignedInteger('finished_at')->nullable(); // Ubah menjadi unsignedInteger
         });
 
         Schema::create('failed_jobs', function (Blueprint $table) {
             $table->id();
-            $table->string('uuid')->unique();
+            $table->string('uuid', 191)->unique(); // Ubah panjang kolom menjadi 191 karakter
             $table->text('connection');
             $table->text('queue');
             $table->longText('payload');
