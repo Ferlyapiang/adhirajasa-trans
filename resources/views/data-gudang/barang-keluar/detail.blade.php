fc689b654a7e9e88d2e436be5f27452ce713b5aa
ferly.jeremi <ferly.jeremi@mncbank.co.id>
2024-09-06 16:07:11 +0700

    Update detail.blade.php

--- resources/views/data-gudang/barang-keluar/detail.blade.php
+++ resources/views/data-gudang/barang-keluar/detail.blade.php
@@ -85,6 +85,17 @@
                                         @method('PUT')
 
                                         <div class="form-group">
+                                            <label for="nomer_invoice">Nomor Container</label>
+                                            <input type="text" name="nomer_invoice" id="nomer_invoice"
+                                                class="form-control @error('nomer_invoice') is-invalid @enderror"
+                                                value="{{ old('nomer_invoice', $barangKeluar->nomer_invoice) }}"
+                                                readonly>
+                                            @error('nomer_invoice')
+                                                <span class="invalid-feedback">{{ $message }}</span>
+                                            @enderror
+                                        </div>
+
+                                        <div class="form-group">
                                             <label for="tanggal_keluar">Tanggal Keluar</label>
                                             <input type="date" name="tanggal_keluar" id="tanggal_keluar"
                                                 class="form-control @error('tanggal_keluar') is-invalid @enderror"
@@ -125,17 +136,6 @@
                                                 @endforeach
                                             </select>
                                             @error('customer_id')
-                                                <span class="invalid-feedback">{{ $message }}</span>
-                                            @enderror
-                                        </div>
-
-                                        <div class="form-group">
-                                            <label for="nomer_container">Nomor Container</label>
-                                            <input type="text" name="nomer_container" id="nomer_container"
-                                                class="form-control @error('nomer_container') is-invalid @enderror"
-                                                value="{{ old('nomer_container', $barangKeluar->nomer_container) }}"
-                                                readonly>
-                                            @error('nomer_container')
                                                 <span class="invalid-feedback">{{ $message }}</span>
                                             @enderror
                                         </div>
