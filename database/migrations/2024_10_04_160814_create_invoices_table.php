f8ca4acafcb5c5a3aac8532aaaaec435607be2c2
Ferly Jeremi Purnawan Apiang <ferly.apiang9@gmail.com>
2024-10-15 23:01:35 +0700

    Update 2024_10_04_160814_create_invoices_table.php

--- database/migrations/2024_10_04_160814_create_invoices_table.php
+++ database/migrations/2024_10_04_160814_create_invoices_table.php
@@ -18,7 +18,9 @@ class CreateInvoicesTable extends Migration
             $table->string('nomer_invoice')->nullable();
             $table->foreignId('barang_masuks_id')->nullable()->constrained('barang_masuks')->onDelete('set null'); // Foreign key to barang_masuks table, nullable
             $table->foreignId('barang_keluars_id')->nullable()->constrained('barang_keluars')->onDelete('set null'); // Foreign key to barang_keluars table, nullable
-            $table->timestamps(); // Created at and updated at timestamps
+            $table->date('tanggal_masuk')->nullable();
+            $table->integer('diskon')->nullable();
+            $table->timestamps();
         });
     }
 
